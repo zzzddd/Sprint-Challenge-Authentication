238ba4656bf6dc90b13044888333c78fd8c45bd9
zee <zzdd9251@gmail.com>
2019-11-15 10:52:39 -0800

    testing for routes added.. added....

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,7 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
-
+const jwt = require("jsonwebtoken"); 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
 
